5bc417a9c5825e3f1197614cb05add5069aa5912
Jacinta Ferrant <jacinta@trustmachines.co>
2023-08-07 09:52:09 -0400

    CRC: make get_signers and get_coordinator_public_keys into trait
    Signed-off-by: Jacinta Ferrant <jacinta@trustmachines.co>

--- sbtc-core/src/signer/mod.rs
+++ sbtc-core/src/signer/mod.rs
@@ -3,40 +3,42 @@ pub mod config;
 
 use std::collections::HashMap;
 
-use crate::signer::config::Config;
 use crate::SBTCError;
+use crate::{signer::config::Config, SBTCResult};
 use bitcoin::{Network, PrivateKey, PublicKey, Transaction as BitcoinTransaction};
 use p256k1::ecdsa;
 use url::Url;
 use wsts::{bip340::SchnorrProof, common::Signature};
 
 #[derive(Default, Clone, Debug)]
-pub struct SignersData {
-    pub signers: HashMap<u32, ecdsa::PublicKey>,
+/// Signers' data required for weighted distributed signing
+pub struct Signers {
+    /// Signer id to public key mapping
+    pub signer_ids: HashMap<u32, ecdsa::PublicKey>,
+    /// Vote share to public key mapping
     pub vote_shares: HashMap<u32, ecdsa::PublicKey>,
 }
 
-/// Coordinator algorithm trait
-pub trait CoordinatorAlgorithm {
-    fn generate_sbtc_wallet_public_key(&self) -> Result<PublicKey, SBTCError>;
-    fn run_signing_round(
-        &self,
-        _tx: BitcoinTransaction,
-    ) -> Result<(Signature, SchnorrProof), SBTCError>;
+/// sBTC Signer trait
+pub trait SBTCSigner {
+    fn signers(&self) -> SBTCResult<Signers>;
+    fn coordinator_public_key(&self, _tx: BitcoinTransaction) -> SBTCResult<PublicKey>;
 }
 
-/// Signer algorithm trait
-pub trait SignerAlgorithm: CoordinatorAlgorithm {
-    fn sign_message(&self, message: &[u8]) -> Result<Vec<u8>, SBTCError>;
-    fn verify_message(
-        &self,
-        public_key: &ecdsa::PublicKey,
-        message: &[u8],
-    ) -> Result<bool, SBTCError>;
+/// Coordinator trait
+pub trait Coordinate {
+    fn generate_sbtc_wallet_public_key(&self) -> SBTCResult<PublicKey>;
+    fn run_signing_round(&self, _tx: BitcoinTransaction) -> SBTCResult<(Signature, SchnorrProof)>;
+}
+
+/// Sign trait
+pub trait Sign: Coordinate {
+    fn sign_message(&self, message: &[u8]) -> SBTCResult<Vec<u8>>;
+    fn verify_message(&self, public_key: &ecdsa::PublicKey, message: &[u8]) -> SBTCResult<bool>;
 }
 
 /// sBTC compliant Signer
-pub struct Signer<S: SignerAlgorithm> {
+pub struct Signer<S: Sign> {
     /// Signer configuration
     pub config: Config,
     /// Signer private key
@@ -45,40 +47,30 @@ pub struct Signer<S: SignerAlgorithm> {
     pub network: Network,
     /// The stacks node RPC URL
     pub stacks_node_rpc_url: Url,
-    /// The signing algorithm
-    pub algorithm: S,
+    /// The signer
+    pub signer: S,
 }
 
-impl<S: SignerAlgorithm> Signer<S> {
+impl<S: Sign> Signer<S> {
     /// Create a new signer
     pub fn new(
         config: Config,
         private_key: PrivateKey,
         network: Network,
         stacks_node_rpc_url: Url,
-        algorithm: S,
+        signer: S,
     ) -> Self {
         Self {
             config,
             private_key,
             network,
             stacks_node_rpc_url,
-            algorithm,
+            signer,
         }
     }
 
-    /// Get the current coordinator public key from the stacks node
-    pub fn get_coordinator_public_key(&self) -> Result<PublicKey, SBTCError> {
-        todo!()
-    }
-
-    /// Retrieve the current signers' data from the stacks node
-    pub fn get_signers_data(&self) -> Result<SignersData, SBTCError> {
-        todo!()
-    }
-
     /// Sign approve the given transaction
-    pub fn approve(&self, _tx: BitcoinTransaction) -> Result<(), SBTCError> {
+    pub fn approve(&self, _tx: BitcoinTransaction) -> SBTCResult<()> {
         todo!()
     }
 
@@ -88,32 +80,37 @@ impl<S: SignerAlgorithm> Signer<S> {
     }
 }
 
-impl<S: SignerAlgorithm> CoordinatorAlgorithm for Signer<S> {
-    /// Generate the sBTC wallet public key
-    fn generate_sbtc_wallet_public_key(&self) -> Result<PublicKey, SBTCError> {
+impl<S: Sign> SBTCSigner for Signer<S> {
+    /// Retrieve the current signers
+    fn signers(&self) -> SBTCResult<Signers> {
         todo!()
     }
 
-    /// Run the signing round for the transaction
-    fn run_signing_round(
-        &self,
-        _tx: BitcoinTransaction,
-    ) -> Result<(Signature, SchnorrProof), SBTCError> {
+    /// Get the current coordinator public key for the given transaction
+    fn coordinator_public_key(&self, _tx: BitcoinTransaction) -> SBTCResult<PublicKey> {
         todo!()
     }
 }
 
-impl<S: SignerAlgorithm> SignerAlgorithm for Signer<S> {
+impl<S: Sign> Coordinate for Signer<S> {
+    /// Generate the sBTC wallet public key
+    fn generate_sbtc_wallet_public_key(&self) -> SBTCResult<PublicKey> {
+        todo!()
+    }
+
+    /// Run the signing round for the transaction
+    fn run_signing_round(&self, _tx: BitcoinTransaction) -> SBTCResult<(Signature, SchnorrProof)> {
+        todo!()
+    }
+}
+
+impl<S: Sign> Sign for Signer<S> {
     /// Sign the given message
-    fn sign_message(&self, _message: &[u8]) -> Result<Vec<u8>, SBTCError> {
+    fn sign_message(&self, _message: &[u8]) -> SBTCResult<Vec<u8>> {
         todo!()
     }
     /// Verify the message was signed by the given public key
-    fn verify_message(
-        &self,
-        _public_key: &ecdsa::PublicKey,
-        _message: &[u8],
-    ) -> Result<bool, SBTCError> {
+    fn verify_message(&self, _public_key: &ecdsa::PublicKey, _message: &[u8]) -> SBTCResult<bool> {
         todo!()
     }
 }
