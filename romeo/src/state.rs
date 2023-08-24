d12cb68e31cf5e82b8e18192b7e2db774881aa24
Stjepan Golemac <stjepan.golemac@gmail.com>
2023-08-24 12:05:14 +0200

    wip: define bitcoin tx update flow

--- romeo/src/state.rs
+++ romeo/src/state.rs
@@ -46,7 +46,7 @@ struct Deposit {
 }
 
 impl Deposit {
-    fn process(&mut self, block_height: u64) -> Option<Task> {
+    fn mint(&mut self, block_height: u64) -> Option<Task> {
         todo!();
     }
 }
@@ -61,10 +61,22 @@ struct Withdrawal {
     fulfillment: Option<Response<BitcoinTransaction>>,
 }
 
+impl Withdrawal {
+    fn burn(&mut self, block_height: u64) -> Option<Task> {
+        todo!();
+    }
+
+    fn fulfill(&mut self, block_height: u64) -> Option<Task> {
+        todo!();
+    }
+}
+
 pub fn update(config: &Config, state: State, event: Event) -> (State, Vec<Task>) {
     match event {
         Event::StacksTransactionUpdate(_, _) => todo!(),
-        Event::BitcoinTransactionUpdate(_, _) => todo!(),
+        Event::BitcoinTransactionUpdate(txid, status) => {
+            process_bitcoin_transaction(config, state, txid, status)
+        }
         Event::BitcoinBlock(block) => process_bitcoin_block(config, state, block),
     }
 }
@@ -80,16 +92,28 @@ fn process_bitcoin_block(config: &Config, mut state: State, block: Block) -> (St
         .bip34_block_height()
         .expect("Failed to get block height");
 
-    let task = Task::FetchBitcoinBlock(state.block_height + 1);
+    let mut tasks = vec![Task::FetchBitcoinBlock(state.block_height + 1)];
 
     let mint_tasks = state
         .deposits
         .iter_mut()
-        .filter_map(|deposit| deposit.process(state.block_height));
+        .filter_map(|deposit| deposit.mint(state.block_height));
 
-    // TODO
+    let burn_tasks = state
+        .withdrawals
+        .iter_mut()
+        .filter_map(|withdrawal| withdrawal.burn(state.block_height));
 
-    unimplemented!()
+    let fulfillment_tasks = state
+        .withdrawals
+        .iter_mut()
+        .filter_map(|withdrawal| withdrawal.fulfill(state.block_height));
+
+    tasks.extend(mint_tasks);
+    tasks.extend(burn_tasks);
+    tasks.extend(fulfillment_tasks);
+
+    (state, tasks)
 }
 
 fn parse_deposits(block: &Block) -> Vec<Deposit> {
@@ -100,4 +124,24 @@ fn parse_deposits(block: &Block) -> Vec<Deposit> {
 fn parse_withdrawals(block: &Block) -> Vec<Withdrawal> {
     // TODO
     vec![]
+}
+
+fn process_bitcoin_transaction(
+    config: &Config,
+    mut state: State,
+    txid: BitcoinTxId,
+    status: TransactionStatus,
+) -> (State, Vec<Task>) {
+    if let Some(fulfillment) = state.withdrawals.iter_mut().find_map(|withdrawal| {
+        withdrawal
+            .fulfillment
+            .filter(|fulfillment| fulfillment.tx.txid() == txid)
+            .as_mut()
+    }) {
+        fulfillment.status = status;
+    };
+
+    // TODO: handle rejections and remove excess state on confirmations
+
+    (state, vec![])
 }
