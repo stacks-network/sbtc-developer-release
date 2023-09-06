f212dd64e3389e28d8297f779e739bb24f33b859
friedger <mail@friedger.de>
2023-09-06 10:50:41 +0200

    fix: use version 2

--- romeo/src/proof_data.rs
+++ romeo/src/proof_data.rs
@@ -47,7 +47,7 @@ impl ProofData {
         let merkle_tree = MerkleTree::<DoubleSha256>::new(&txid_vecs);
 
         let merkle_path = merkle_tree
-            .path(&*tx.txid())
+            .path(&tx.txid())
             .expect("Failed to get merkle path");
 
         let merkle_tree_depth = merkle_path.len();
@@ -102,7 +102,7 @@ mod tests {
     #[test]
     fn should_create_correct_proof_data() {
         // testnet block 100,000
-        let block_hex = "01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff3703a08601000427f1001c046a510100522cfabe6d6d0000000000000000000068692066726f6d20706f6f6c7365727665726aac1eeeed88ffffffff0100f2052a010000001976a914912e2b234f941f30b18afbb4fa46171214bf66c888ac00000000";
+        let block_hex = "0200000035ab154183570282ce9afc0b494c9fc6a3cfea05aa8c1add2ecc56490000000038ba3d78e4500a5a7570dbe61960398add4410d278b21cd9708e6d9743f374d544fc055227f1001c29c1ea3b0101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff3703a08601000427f1001c046a510100522cfabe6d6d0000000000000000000068692066726f6d20706f6f6c7365727665726aac1eeeed88ffffffff0100f2052a010000001976a914912e2b234f941f30b18afbb4fa46171214bf66c888ac00000000";
         let block: Block = deserialize(&Vec::<u8>::from_hex(block_hex).unwrap()).unwrap();
         let block_height = 100000;
         let hash = "00000000009e2958c15ff9290d571bf9459e93b19765c6801ddeccadbb160a1e";
