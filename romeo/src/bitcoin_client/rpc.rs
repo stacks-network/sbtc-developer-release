e51aa4251e7616f6cee41ff1543d0bb2aa422ec5
Stjepan Golemac <stjepan.golemac@gmail.com>
2023-09-12 16:01:39 +0200

    feat: start work on rpc bitcoin client

--- romeo/src/bitcoin_client/rpc.rs
+++ romeo/src/bitcoin_client/rpc.rs
@@ -1,1 +1,25 @@
 //! RPC Bitcoin client
+
+use bdk::bitcoincore_rpc::{Auth, Client};
+use url::Url;
+
+pub struct RPCClient(Client);
+
+impl RPCClient {
+    pub fn new(mut url: Url) -> anyhow::Result<Self> {
+        let username = url.username().to_string();
+
+        if username.is_empty() {
+            return Err(anyhow::anyhow!("Username is empty"));
+        }
+
+        let password = url.password().unwrap_or_default().to_string();
+
+        url.set_username("").unwrap();
+        url.set_password(None).unwrap();
+
+        let client = Client::new(&url.to_string(), Auth::UserPass(username, password))?;
+
+        Ok(Self(client))
+    }
+}
