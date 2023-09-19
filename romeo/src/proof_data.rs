dbe5209e087ec196181282e64d769844ae8fc2d5
Friedger Müffke <friedger@gmail.com>
2023-09-19 14:08:27 +0200

    Use rs_merkle (#147)
    * fix: use rs_merkle
    
    * fix: re-add test data
    
    * chore: format
    
    * chore: remove prints, improve tests
    
    * chore: use hasher from stacks_core, more comments

--- romeo/src/proof_data.rs
+++ romeo/src/proof_data.rs
@@ -1,14 +1,33 @@
 //! Proof Data used in Clarity Contracts
 use bdk::bitcoin::{Block, BlockHeader, Transaction, Txid as BitcoinTxId};
-use blockstack_lib::util::hash::{DoubleSha256, MerklePath, MerkleTree};
 use blockstack_lib::vm::types::{ListData, ListTypeData, SequenceData, Value, BUFF_32};
+use rs_merkle::{Hasher, MerkleTree};
+use stacks_core::crypto::sha256::DoubleSha256Hasher;
+use stacks_core::crypto::Hashing;
+/// The double sha256 algorithm used for bitcoin
+#[derive(Clone)]
+pub struct DoubleSha256Algorithm {}
+
+impl Hasher for DoubleSha256Algorithm {
+    type Hash = [u8; 32];
+
+    fn hash(data: &[u8]) -> [u8; 32] {
+        DoubleSha256Hasher::hash(data)
+            .as_bytes()
+            .try_into()
+            .unwrap()
+    }
+}
 
 /// Data needed to prove that a bitcoin transaction was mined on the bitcoin
 /// network. This data is used by clarity contracts.
 #[derive(Debug, Clone)]
 pub struct ProofData {
-    /// The transaction id of the bitcoin transaction
-    pub txid: BitcoinTxId,
+    /// The reversed transaction id of the bitcoin transaction
+    /// in little endian format
+    /// as it is produced by bdk crate.
+    /// It is the reversed txid of the one seen in explorers.
+    pub reversed_txid: BitcoinTxId,
     /// The index of the bitcoin transaction in the block
     pub tx_index: u32,
     /// The block height of the bitcoin transaction
@@ -16,9 +35,11 @@ pub struct ProofData {
     /// The block hash of the bitcoin transaction
     pub block_header: BlockHeader,
     /// The path of the bitcoin transaction in the merkle tree
-    pub merkle_path: MerklePath<DoubleSha256>,
+    pub merkle_path: Vec<Vec<u8>>,
     /// The depth of the merkle tree
     pub merkle_tree_depth: u32,
+    /// merkle root
+    pub merkle_root: String,
 }
 
 /// Clarity values for the proof data
@@ -41,26 +62,45 @@ impl ProofData {
     /// Create a new proof from a bitcoin transaction and a block
     pub fn from_block_and_index(block: &Block, index: usize) -> Self {
         let tx: &Transaction = block.txdata.get(index).expect("Invalid tx index");
+        let mut merkle_tree = MerkleTree::<DoubleSha256Algorithm>::new();
+        for tx in &block.txdata {
+            merkle_tree.insert(tx.txid().to_vec().try_into().unwrap());
+        }
+        // append last tx id if number of leaves is odd
+        if block.txdata.len() % 2 == 1 {
+            merkle_tree.insert(
+                block
+                    .txdata
+                    .last()
+                    .unwrap()
+                    .txid()
+                    .to_vec()
+                    .try_into()
+                    .unwrap(),
+            );
+        }
+        merkle_tree.commit();
+        let merkle_path = merkle_tree.proof(&[index]);
 
-        let txid_vecs = block.txdata.iter().map(|tx| tx.txid().to_vec()).collect();
-
-        let merkle_tree = MerkleTree::<DoubleSha256>::new(&txid_vecs);
-
-        let merkle_path = merkle_tree
-            .path(&tx.txid())
-            .expect("Failed to get merkle path");
-
-        let merkle_tree_depth = merkle_path.len();
+        // rs_merkle tree depth counts leaves as well
+        // we only care about the layers above
+        // therefore minus 1.
+        let merkle_tree_depth = merkle_tree.depth() - 1;
 
         Self {
-            txid: tx.txid(),
+            reversed_txid: tx.txid(),
             tx_index: index as u32,
             block_height: block
                 .bip34_block_height()
                 .expect("Failed to get block height"),
             block_header: block.header,
-            merkle_path,
+            merkle_path: merkle_path
+                .proof_hashes()
+                .iter()
+                .map(|h| h.to_vec())
+                .collect(),
             merkle_tree_depth: merkle_tree_depth as u32,
+            merkle_root: hex::encode(merkle_tree.root().unwrap()),
         }
     }
 
@@ -73,8 +113,11 @@ impl ProofData {
         header.append(&mut self.block_header.bits.to_le_bytes().to_vec());
         header.append(&mut self.block_header.nonce.to_le_bytes().to_vec());
 
+        // use txid in big endian for clarity call
+        let mut txid = self.reversed_txid.to_vec();
+        txid.reverse();
         ProofDataClarityValues {
-            txid: Value::buff_from(self.txid.to_vec()).expect("Failed to convert txid to buffer"),
+            txid: Value::buff_from(txid).expect("Failed to convert txid to buffer"),
             tx_index: Value::UInt(self.tx_index as u128),
             block_height: Value::UInt(self.block_height as u128),
             block_header: Value::buff_from(header)
@@ -83,7 +126,7 @@ impl ProofData {
                 data: self
                     .merkle_path
                     .iter()
-                    .map(|v| Value::buff_from(v.hash.to_bytes().to_vec()).unwrap())
+                    .map(|v| Value::buff_from(v.clone()).unwrap())
                     .collect(),
                 type_signature: ListTypeData::new_list(BUFF_32.clone(), 14).unwrap(),
             })),
@@ -112,7 +155,7 @@ mod tests {
         let proof_data = ProofData::from_block_and_index(&block, txindex);
 
         assert_eq!(proof_data.block_height, block_height);
-        assert_eq!(proof_data.txid.to_string(), txid);
+        assert_eq!(proof_data.reversed_txid.to_string(), txid);
         assert_eq!(proof_data.block_header.block_hash().to_string(), hash);
     }
 
@@ -141,6 +184,7 @@ mod tests {
 
     #[test]
     fn should_convert_to_clarity_values() {
+        // testnet block 100,000
         let block_hex = "0200000035ab154183570282ce9afc0b494c9fc6a3cfea05aa8c1add2ecc56490000000038ba3d78e4500a5a7570dbe61960398add4410d278b21cd9708e6d9743f374d544fc055227f1001c29c1ea3b0101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff3703a08601000427f1001c046a510100522cfabe6d6d0000000000000000000068692066726f6d20706f6f6c7365727665726aac1eeeed88ffffffff0100f2052a010000001976a914912e2b234f941f30b18afbb4fa46171214bf66c888ac00000000";
         let block: Block = deserialize(&Vec::<u8>::from_hex(block_hex).unwrap()).unwrap();
         let txindex: usize = 0;
@@ -148,10 +192,29 @@ mod tests {
         let values = proof_data.to_values();
         assert_eq!(values.block_header.to_string(), "0x0200000035ab154183570282ce9afc0b494c9fc6a3cfea05aa8c1add2ecc56490000000038ba3d78e4500a5a7570dbe61960398add4410d278b21cd9708e6d9743f374d544fc055227f1001c29c1ea3b");
         assert_eq!(values.block_height.to_string(), "u100000");
+        assert_eq!(values.merkle_tree_depth.to_string(), "u1");
         assert_eq!(
             values.merkle_path.to_string(),
-            "(0xd6141b363505039cdb97b4552766872ad925b63be83cbb4bc286fe9970362242)"
+            "(0x38ba3d78e4500a5a7570dbe61960398add4410d278b21cd9708e6d9743f374d5)"
         );
-        assert_eq!(values.merkle_tree_depth.to_string(), "u1");
+    }
+
+    // test from_block_and_index returns correct proof
+    // block taken from local regtest node
+    #[test]
+    fn should_create_correct_merkle_root() {
+        let block_hex = "000000205214e3b1be1007826f4537f7d86d8f890104587beae37af2fb17e31195a62325bb8940196d4479391e3460fcc904963da6726ecbb99cb9dfc3705ad9ba748f2182270865ffff7f200000000003020000000001010000000000000000000000000000000000000000000000000000000000000000ffffffff0402d20d00ffffffff029f470000000000001976a914ee9369fb719c0ba43ddf4d94638a970b84775f4788ac0000000000000000266a24aa21a9ed2bec0280b5488f0dd3cca56932fdc3eb7b7fba766f6819a0de1cfeaf74c61ecb0120000000000000000000000000000000000000000000000000000000000000000000000000010000000131bde99ad6d1edb8a0f25b7f8458e605e6c1725217346757e7c9a4c4365ef634030000006b483045022100af3c5c67e972b3c744309e79476d71b8c408ce1e45891b3f4d0a9b8bb76c3a8f0220132e8850d7573747a83ccd0b969f12056b8e960a7e6556f389a87b9be218fc2301210239810ebf35e6f6c26062c99f3e183708d377720617c90a986859ec9c95d00be9fdffffff040000000000000000536a4c5069645b76f4413c41080e57ba4b01a485dc7d2465051bfbd2c97f419ddace3e993f88be7b621278694299a79abc623dd56d071f01245e8648e141bfec88d9ba3b1deef100000dd1000100000ab900014a10270000000000001976a914000000000000000000000000000000000000000088ac10270000000000001976a914000000000000000000000000000000000000000088ac82b0c524010000001976a914ee9369fb719c0ba43ddf4d94638a970b84775f4788ac0000000001000000000101010da73321be48f30562e44ff379ea981e204a4fa4bc859c6cd99418e705c7390000000000feffffff0300000000000000001b6a1969643c051a6d78de7b0625dfbfc16c3a8a5735f6dc3dc3f2cee8030000000000002251205e682db7c014ab76f2b4fdcbbdb76f9b8111468174cdb159df6e88fe9d078ce6ab040000000000001600148ae4a48cb0c3b7874460a6f5287d9dd512a182460247304402206387c555478eb821311ef4d3b125a8b4beb698be624e186ff6234f6cd1deb75702207cf063c9cd57dcd7c34b9477129a3a70403856a46be7b9e8942d79482b246379012103ab37f5b606931d7828855affe75199d952bc6174b4a23861b7ac94132210508cc10d0000";
+        let block: Block = deserialize(&Vec::<u8>::from_hex(block_hex).unwrap()).unwrap();
+        let txindex: usize = 0;
+        let proof_data = ProofData::from_block_and_index(&block, txindex);
+        let values = proof_data.to_values();
+        assert_eq!(values.block_header.to_string(), "0x000000205214e3b1be1007826f4537f7d86d8f890104587beae37af2fb17e31195a62325bb8940196d4479391e3460fcc904963da6726ecbb99cb9dfc3705ad9ba748f2182270865ffff7f2000000000");
+        assert_eq!(values.block_height.to_string(), "u3538");
+        assert_eq!(values.merkle_tree_depth.to_string(), "u2");
+        assert_eq!(values.merkle_path.to_string(), "(0x30955a1f27461b4ca06d68147a377a585d05499d186853a2e05e21cf4f9bf55f 0xb4a7cc817198247161027ab3584b0c6a1bd2f7319d6468d2c6e128ec3acb2a47)");
+        assert_eq!(
+            values.txid.to_string(),
+            "0xd564f1a4e53e7bad92f67c9a05b748e504ac1b8155db4c2d9b4ed12afd32139f"
+        )
     }
 }
