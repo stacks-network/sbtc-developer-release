b73c9ae542ec2e3930a5d341e4e044b987d8d117
Friedger Müffke <friedger@gmail.com>
2023-10-03 16:39:37 +0200

    Update romeo/asset-contract/tests/clarity-bitcoin-mini-deploy.test.ts
    Co-authored-by: Hugo C <911307+hugocaillard@users.noreply.github.com>

--- romeo/asset-contract/tests/clarity-bitcoin-mini-deploy.test.ts
+++ romeo/asset-contract/tests/clarity-bitcoin-mini-deploy.test.ts
@@ -1,8 +1,5 @@
 import { beforeEach, describe, expect, it } from "vitest";
 
-beforeEach(async () => {
-  await vm.initSession(process.cwd(), "./Clarinet.toml");
-});
 
 describe("test deploy version and unit test version of clarity-bitcoin-mini", () => {
   it("ensures the versions differ only in DEBUG flag", () => {
