83c3bfecc654adee5e5dd6b8b6eb500ed4b7fcfa
Carlos Alejandro Gutierrez Sandoval <calgusa@gmail.com>
2023-10-05 16:02:11 -0600

    Integration: mine empty block

---
+++ romeo/tests/bitcoin_client_integration.rs
@@ -0,0 +1,50 @@
+use std::env;
+
+use reqwest::blocking::Client;
+use serde_json::json;
+use url::Url;
+
+pub fn project_name() -> String {
+	env::var("PROJECT_NAME").unwrap()
+}
+
+pub fn bitcoin_url() -> Url {
+	let base = project_name();
+	Url::parse(&format!("http://{base}-bitcoin-1:18443")).unwrap()
+}
+
+pub fn electrs_url() -> Url {
+	let base = project_name();
+	Url::parse(&format!("tcp://{base}-electrs-1:60401")).unwrap()
+}
+
+pub fn generate_blocks(blocks: u64, ctx: &Client, address: &str) {
+	let endpoint = bitcoin_url();
+	let user = "devnet";
+	let password = "devnet";
+	let body = json!({
+		"jsonrpc": "1.0",
+		"id": "1",
+		"method": "generatetoaddress",
+		//developer's
+		"params": [blocks,address]
+	});
+
+	let response_json: serde_json::Value = ctx
+		.post(endpoint)
+		.basic_auth(user, Some(password))
+		.header(reqwest::header::CONTENT_TYPE, "application/json")
+		.json(&body)
+		.send()
+		.unwrap()
+		.json()
+		.unwrap();
+
+	assert_eq!(response_json["error"], serde_json::Value::Null);
+}
+
+#[test]
+fn mine_empty_block() {
+	let client = Client::new();
+	generate_blocks(10, &client, "mqVnk6NPRdhntvfm4hh9vvjiRkFDUuSYsH");
+}
