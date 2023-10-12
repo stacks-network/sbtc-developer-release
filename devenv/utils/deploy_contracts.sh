76958af76cf079c7b4476dbada5792804378c763
Friedger <friedger@gmail.com>
2023-10-12 12:31:54 +0200

    feat: wait until stacks chain is live

--- devenv/utils/deploy_contracts.sh
+++ devenv/utils/deploy_contracts.sh
@@ -3,6 +3,22 @@
 # Deploys the asset contract to the devnet
 
 SCRIPT_DIR=$(dirname "$0")
+API_URL=http://localhost:20443/v2/info
+
+echo "Waiting on Stacks API $API_URL"
+while ! curl -s $API_URL >/dev/null; do
+    sleep 1
+done
+
+# stacks ready to take contracts
+
+STACKS_HEIGHT=1
+echo "Waiting on Stacks height $STACKS_HEIGHT"
+while [ "$(curl -s $API_URL | jq '.stacks_tip_height')" -lt $STACKS_HEIGHT ]; do
+    sleep 2
+done
+
+# deploy the contracts
 
 cd $SCRIPT_DIR/../../romeo/asset-contract && \
     clarinet deployments apply -p deployments/default.devnet-plan.yaml && \
