451589e96d202e738882bcacdf0e98fc9890d82c
Carlos Alejandro Gutierrez Sandoval <calgusa@gmail.com>
2023-10-16 17:22:58 -0600

    don't build in `up.sh`

--- devenv/up.sh
+++ devenv/up.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-./build.sh
-if [ $? -eq 0 ] 
-then
-  docker compose up -d
+if [ $? -eq 0 ]; then
+    docker compose up -d
 else
-  echo "Build failed, not starting devenv"
+    echo "Build failed, not starting devenv"
 fi
