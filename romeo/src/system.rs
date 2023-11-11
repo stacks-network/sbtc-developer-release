e07fd1c2c820cadb1c3832786562b78151448014
Carlos Alejandro Gutierrez Sandoval <calgusa@gmail.com>
2023-11-10 22:18:47 -0600

    lint

--- romeo/src/system.rs
+++ romeo/src/system.rs
@@ -227,8 +227,7 @@ async fn update_contract_public_key(
 			.bitcoin_credentials
 			.public_key_p2tr()
 			.serialize()
-			.try_into()
-			.unwrap(),
+			.into(),
 	)
 	.expect("Cannot convert public key into a Clarity Value")];
 
