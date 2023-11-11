cliqueengagements/sbtc

init deposit test

romeo/tests/deposit_integration.rs
@@ -1,0 +1,101 @@
+use std::{thread::sleep, time::Duration};
+
+use anyhow::Result;
+use bdk::bitcoin::psbt::serialize::Serialize;
+use reqwest::blocking::Client;
+use sbtc_cli::commands::{
+	broadcast::{broadcast_tx, BroadcastArgs},
+	deposit::{build_deposit_tx, DepositArgs},
+};
+
+mod bitcoin_client_integration;
+
+use bitcoin_client_integration::{electrs_url, generate_blocks};
+
+const WALLET_0_P2TR_ADDRESS: &str =
+	"bcrt1pte5zmd7qzj4hdu45lh9mmdm0nwq3z35pwnxmzkwld6y0a8g83nnqhj6vc0";
+const WALLET_0_P2WPKH_ADDRESS: &str =
+	"bcrt1q3zl64vadtuh3vnsuhdgv6pm93n82ye8q6cr4ch";
+const WALLET_1_P2WPKH_WIF: &str =
+	"cNcXK2r8bNdWJQymtAW8tGS7QHNtFFvG5CdXqhhT752u29WspXRM";
+const WALLET_1_STX_ADDRESS: &str = "ST2ST2H80NP5C9SPR4ENJ1Z9CDM9PKAJVPYWPQZ50";
+const WALLET_1_P2WPKH_ADDRESS: &str =
+	"bcrt1q3tj2fr9scwmcw3rq5m6jslva65f2rqjxfrjz47";
+
+use bdk::{
+	bitcoin::PrivateKey,
+	blockchain::{
+		ConfigurableBlockchain, ElectrumBlockchain, ElectrumBlockchainConfig,
+	},
+	database::MemoryDatabase,
+	template::P2Wpkh,
+	SyncOptions, Wallet,
+};
+
+#[test]
+fn broadcast_deposit() -> Result<()> {
+	let client = Client::new();
+	{
+		generate_blocks(1, &client, WALLET_1_P2WPKH_ADDRESS);
+		generate_blocks(1, &client, WALLET_0_P2WPKH_ADDRESS);
+		// pads blocks to get rewards.
+		generate_blocks(200, &client, WALLET_1_P2WPKH_ADDRESS);
+	};
+
+	let electrum_url = electrs_url();
+	let amount = 10_000;
+
+	// suboptimal, replace once we have better events.
+	{
+		let private_key = PrivateKey::from_wif(WALLET_1_P2WPKH_WIF)?;
+
+		let blockchain =
+			ElectrumBlockchain::from_config(&ElectrumBlockchainConfig {
+				url: electrum_url.clone().into(),
+				socks5: None,
+				retry: 3,
+				timeout: Some(10),
+				stop_gap: 10,
+				validate_domain: false,
+			})
+			.unwrap();
+
+		let wallet = Wallet::new(
+			P2Wpkh(private_key),
+			Some(P2Wpkh(private_key)),
+			bdk::bitcoin::Network::Regtest,
+			MemoryDatabase::default(),
+		)
+		.unwrap();
+
+		loop {
+			wallet.sync(&blockchain, SyncOptions::default()).unwrap();
+			let balance = wallet.get_balance().unwrap();
+			if balance.confirmed != 0 {
+				break;
+			}
+			sleep(Duration::from_millis(1_000));
+		}
+	}
+
+	let tx = {
+		let args = DepositArgs {
+			node_url: electrum_url.clone(),
+			wif: WALLET_1_P2WPKH_WIF.into(),
+			network: bdk::bitcoin::Network::Regtest,
+			recipient: WALLET_1_STX_ADDRESS.into(),
+			amount,
+			dkg_wallet: WALLET_0_P2TR_ADDRESS.into(),
+		};
+
+		build_deposit_tx(&args).unwrap()
+	};
+
+	broadcast_tx(&BroadcastArgs {
+		node_url: electrum_url,
+		tx: hex::encode(tx.serialize()),
+	})
+	.unwrap();
+
+	Ok(())
+}

sbtc-cli/src/commands/deposit.rs
@@ -1,9 +1,9 @@
-use std::{io::stdout, str::FromStr};
+use std::str::FromStr;
 
 use bdk::{
 	bitcoin::{
-		psbt::serialize::Serialize, Address as BitcoinAddress,
-		Network as BitcoinNetwork, PrivateKey,
+		Address as BitcoinAddress, Network as BitcoinNetwork, PrivateKey,
+		Transaction,
 	},
 	blockchain::{
 		ConfigurableBlockchain, ElectrumBlockchain, ElectrumBlockchainConfig,
@@ -17,36 +17,34 @@ use sbtc_core::operations::op_return::deposit::build_deposit_transaction;
 use stacks_core::address::StacksAddress;
 use url::Url;
 
-use crate::commands::utils;
-
 #[derive(Parser, Debug, Clone)]
 pub struct DepositArgs {
 	/// Where to broadcast the transaction
 	#[clap(short('u'), long)]
-	node_url: Url,
+	pub node_url: Url,
 
 	/// Bitcoin WIF of the P2wPKH address
 	#[clap(short, long)]
-	wif: String,
+	pub wif: String,
 
 	/// Bitcoin network where the deposit will be broadcasted to
 	#[clap(short, long)]
-	network: BitcoinNetwork,
+	pub network: BitcoinNetwork,
 
 	/// Stacks address that will receive sBTC
 	#[clap(short, long)]
-	recipient: String,
+	pub recipient: String,
 
 	/// The amount of sats to send
 	#[clap(short, long)]
-	amount: u64,
+	pub amount: u64,
 
 	/// Dkg wallet address
 	#[clap(short, long)]
-	dkg_wallet: String,
+	pub dkg_wallet: String,
 }
 
-pub fn build_deposit_tx(deposit: &DepositArgs) -> anyhow::Result<()> {
+pub fn build_deposit_tx(deposit: &DepositArgs) -> anyhow::Result<Transaction> {
 	let private_key = PrivateKey::from_wif(&deposit.wif)?;
 
 	let blockchain =
@@ -72,21 +70,12 @@ pub fn build_deposit_tx(deposit: &DepositArgs) -> anyhow::Result<()> {
 		StacksAddress::try_from(deposit.recipient.as_str())?;
 	let dkg_address = BitcoinAddress::from_str(&deposit.dkg_wallet)?;
 
-	let tx = build_deposit_transaction(
+	build_deposit_transaction(
 		wallet,
 		recipient_address.into(),
 		dkg_address,
 		deposit.amount,
 		deposit.network,
-	)?;
-
-	serde_json::to_writer_pretty(
-		stdout(),
-		&utils::TransactionData {
-			id: tx.txid().to_string(),
-			hex: hex::encode(tx.serialize()),
-		},
-	)?;
-
-	Ok(())
+	)
+	.map_err(|e| e.into())
 }

stacks-network/sbtc-developer-release

romeo/tests/deposit_integration.rs
@@ -1,0 +1,104 @@
+use std::{thread::sleep, time::Duration};
+
+use anyhow::Result;
+use bdk::bitcoin::psbt::serialize::Serialize;
+use reqwest::blocking::Client;
+use sbtc_cli::commands::{
+	broadcast::{broadcast_tx, BroadcastArgs},
+	deposit::{build_deposit_tx, DepositArgs},
+};
+
+mod bitcoin_client_integration;
+
+use bitcoin_client_integration::{electrs_url, generate_blocks};
+
+const WALLET_0_P2TR_ADDRESS: &str =
+	"bcrt1pte5zmd7qzj4hdu45lh9mmdm0nwq3z35pwnxmzkwld6y0a8g83nnqhj6vc0";
+const WALLET_0_P2WPKH_ADDRESS: &str =
+	"bcrt1q3tj2fr9scwmcw3rq5m6jslva65f2rqjxfrjz47";
+const WALLET_0_P2WPKH_WIF: &str =
+	"cTyHitzs3VRnNxrpwxo3fXTTe569wHNUs57tQM7Z1FrzUDNB5mqm";
+const WALLET_1_P2WPKH_WIF: &str =
+	"cNcXK2r8bNdWJQymtAW8tGS7QHNtFFvG5CdXqhhT752u29WspXRM";
+const WALLET_1_STX_ADDRESS: &str = "ST2ST2H80NP5C9SPR4ENJ1Z9CDM9PKAJVPYWPQZ50";
+const WALLET_1_P2WPKH_ADDRESS: &str =
+	"bcrt1q3zl64vadtuh3vnsuhdgv6pm93n82ye8q6cr4ch";
+
+use bdk::{
+	bitcoin::PrivateKey,
+	blockchain::{
+		ConfigurableBlockchain, ElectrumBlockchain, ElectrumBlockchainConfig,
+	},
+	database::MemoryDatabase,
+	template::P2Wpkh,
+	SyncOptions, Wallet,
+};
+
+#[test]
+fn broadcast_deposit() -> Result<()> {
+	let client = Client::new();
+	{
+		generate_blocks(1, &client, WALLET_0_P2WPKH_ADDRESS);
+		generate_blocks(1, &client, WALLET_1_P2WPKH_ADDRESS);
+		// pads blocks to get rewards.
+		generate_blocks(200, &client, WALLET_0_P2WPKH_ADDRESS);
+	};
+
+	let electrum_url = electrs_url();
+
+	// suboptimal, replace once we have better events.
+	{
+		let blockchain =
+			ElectrumBlockchain::from_config(&ElectrumBlockchainConfig {
+				url: electrum_url.clone().into(),
+				socks5: None,
+				retry: 3,
+				timeout: Some(10),
+				stop_gap: 10,
+				validate_domain: false,
+			})
+			.unwrap();
+
+		let private_key = PrivateKey::from_wif(WALLET_1_P2WPKH_WIF)?;
+
+		let wallet = Wallet::new(
+			P2Wpkh(private_key),
+			Some(P2Wpkh(private_key)),
+			bdk::bitcoin::Network::Regtest,
+			MemoryDatabase::default(),
+		)
+		.unwrap();
+
+		loop {
+			wallet.sync(&blockchain, SyncOptions::default()).unwrap();
+			let balance = wallet.get_balance().unwrap();
+			if balance.confirmed != 0 {
+				break;
+			}
+			sleep(Duration::from_millis(1_000));
+		}
+	}
+
+	let amount = 10_000;
+
+	let tx = {
+		let args = DepositArgs {
+			node_url: electrum_url.clone(),
+			wif: WALLET_1_P2WPKH_WIF.into(),
+			network: bdk::bitcoin::Network::Regtest,
+			recipient: WALLET_1_STX_ADDRESS.into(),
+			amount,
+			sbtc_wallet: WALLET_0_P2TR_ADDRESS.into(),
+		};
+
+		build_deposit_tx(&args).unwrap()
+	};
+
+	broadcast_tx(&BroadcastArgs {
+		node_url: electrum_url,
+		tx: hex::encode(tx.serialize()),
+	})
+	.unwrap();
+
+	Ok(())
+}

sbtc-cli/src/commands/deposit.rs
@@ -1,9 +1,9 @@
-use std::{io::stdout, str::FromStr};
+use std::str::FromStr;
 
 use bdk::{
 	bitcoin::{
-		psbt::serialize::Serialize, Address as BitcoinAddress,
-		Network as BitcoinNetwork, PrivateKey,
+		Address as BitcoinAddress, Network as BitcoinNetwork, PrivateKey,
+		Transaction,
 	},
 	blockchain::{
 		ConfigurableBlockchain, ElectrumBlockchain, ElectrumBlockchainConfig,
@@ -17,36 +17,34 @@ use sbtc_core::operations::op_return::deposit::build_deposit_transaction;
 use stacks_core::utils::PrincipalData;
 use url::Url;
 
-use crate::commands::utils;
-
 #[derive(Parser, Debug, Clone)]
 pub struct DepositArgs {
 	/// Where to broadcast the transaction
 	#[clap(short('u'), long)]
-	node_url: Url,
+	pub node_url: Url,
 
 	/// Bitcoin WIF of the P2wPKH address
 	#[clap(short, long)]
-	wif: String,
+	pub wif: String,
 
 	/// Bitcoin network where the deposit will be broadcasted to
 	#[clap(short, long)]
-	network: BitcoinNetwork,
+	pub network: BitcoinNetwork,
 
 	/// Stacks address that will receive sBTC
 	#[clap(short, long)]
-	recipient: String,
+	pub recipient: String,
 
 	/// The amount of sats to send
 	#[clap(short, long)]
-	amount: u64,
+	pub amount: u64,
 
 	/// Bitcoin address of the sbtc wallet
 	#[clap(short, long)]
-	sbtc_wallet: String,
+	pub sbtc_wallet: String,
 }
 
-pub fn build_deposit_tx(deposit: &DepositArgs) -> anyhow::Result<()> {
+pub fn build_deposit_tx(deposit: &DepositArgs) -> anyhow::Result<Transaction> {
 	let private_key = PrivateKey::from_wif(&deposit.wif)?;
 
 	let blockchain =
@@ -71,21 +69,12 @@ pub fn build_deposit_tx(deposit: &DepositArgs) -> anyhow::Result<()> {
 	let stx_recipient = PrincipalData::try_from(deposit.recipient.to_string())?;
 	let sbtc_wallet_address = BitcoinAddress::from_str(&deposit.sbtc_wallet)?;
 
-	let tx = build_deposit_transaction(
+	build_deposit_transaction(
 		wallet,
 		stx_recipient,
 		sbtc_wallet_address,
 		deposit.amount,
 		deposit.network,
-	)?;
-
-	serde_json::to_writer_pretty(
-		stdout(),
-		&utils::TransactionData {
-			id: tx.txid().to_string(),
-			hex: hex::encode(tx.serialize()),
-		},
-	)?;
-
-	Ok(())
+	)
+	.map_err(|e| e.into())
 }